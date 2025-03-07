e155c4ae0a417a52ab756f3b122bf288bf1c5ef0
furkanfurkan1 <lolcool922@gmail.com>
2025-03-07 19:13:18 +0100

    Add files via upload
    Music Product Owner added, Youtube Player

--- imagescaling.js
+++ imagescaling.js
@@ -1,5 +1,7 @@
 window.addEventListener('scroll', function() {
     var scrollPosition = window.scrollY; // Der Wert des Scrollens
-    var scaleFactor = 70 + scrollPosition / 10; // Berechnet den Skalierungsfaktor
+    var scaleFactor = 70 + scrollPosition / 5; // Berechnet den Skalierungsfaktor
     document.body.style.setProperty('--bg-size', scaleFactor + '%'); // Setzt den Wert für background-size
 });
+
+
